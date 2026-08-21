f38ebc5fc44fce295522db4dabc32541dcc0fc1b
Hemanta Bhengra <hemantabhengra8@gmail.com>
2026-08-21 19:54:35 +0530

    week 2

---
+++ Week 2 - DSA  Loops and Pattern Printing/Number_Reverse.cpp
@@ -0,0 +1,15 @@
+// printig number in reverse 
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    while(n != 0){
+        cout << n % 10;
+        n = n/10;
+
+    }
+    return 0;
+}   
