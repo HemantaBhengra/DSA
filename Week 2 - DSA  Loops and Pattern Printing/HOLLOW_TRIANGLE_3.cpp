df96970dc4d0cdf4b6c5cbdee044a04086d61a6d
Hemanta Bhengra <hemantabhengra8@gmail.com>
2026-09-15 16:06:24 +0530

    new_question_added

---
+++ Week 2 - DSA  Loops and Pattern Printing/HOLLOW_TRIANGLE_3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<=(n-i);j++){
+            cout<<" ";
+        }
+        for(int k=1;k<=i;k++){
+            if(i == 1 || i == n || k == 1 || k ==i ){
+                cout<<"* ";
+            }else {
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
